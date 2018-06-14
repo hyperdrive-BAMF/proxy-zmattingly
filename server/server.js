c06bf309a664fb6b41a5b44d351832b4f1312bc6
Zane Mattingly <z@matting.ly>
2018-06-14 15:17:56 -0700

    setup: adding another static dir as customerReviews/public for images

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../customerReviews/public')));
 
 // Splash Carousel
 app.get('/splashpage/:gameId', (req, res) => {
